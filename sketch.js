1898fb27b122273598fd1299699dfc74cfcf4d27
banmedo <ban.medo@live.com>
2017-02-17 14:10:51 +0545

    loaded mapbox light theme whole - page

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
 function setup() {
-
+	//remove the canvas as we are working on the leaflet layer
+	noCanvas(); 
+	
+	//initialize mapbox - using light theme for the instance
+	mapboxgl.accessToken = 'pk.eyJ1IjoiYmFubWVkbyIsImEiOiJhSklqeEZzIn0.rzfSxO3cVUhghA2sJN378A';
+	var map_box = new mapboxgl.Map({
+		container: 'map_box',
+		style: 'mapbox://styles/mapbox/light-v9'
+	});
+	
 }
 
 function draw() {
-  
+	
 }
