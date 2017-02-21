e28ec4a54027267a8ce35cb4787d618b75c9f37e
banmedo <ban.medo@live.com>
2017-02-21 16:58:30 +0545

    started to create a chatbox class

---
+++ chatbox.js
@@ -0,0 +1,18 @@
+Chatbox = function (lat, lng, html){
+	this.lat;
+	this.lng;
+	this.html;
+	this.hidden=false;
+	
+	show = function(){
+		
+	}
+	
+	hide = function(){
+		
+	}
+	
+	adjust = function (chatboxes){
+		
+	}
+}
